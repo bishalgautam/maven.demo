28b85f889ac948fbbb1af53b62577a2dded3bf1f
bishalgautam <bishal.htd04@gmail.com>
2017-04-07 01:37:34 -0400

    done except the shortest interval one

--- src/main/java/maven/demo/Assignment.java
+++ src/main/java/maven/demo/Assignment.java
@@ -1,51 +1,75 @@
 package maven.demo;
 
-//import java.io.BufferedReader;
-//import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
-//import com.google.gson.JsonElement;
-//import com.google.gson.JsonObject;
-//import com.google.gson.JsonParser;
-
 import okhttp3.*;
 
 public class Assignment{
 	
 	private	final OkHttpClient client;
 	private final Gson gson;
+	/*HashSet for Number of unique customers*/
 	private HashSet<BigDecimal> uniqueCust;
-	//private HashMap<BigDecimal, >
-	private List<BigDecimal> totalPrice;
-	//private BigDecimal[] countTotal;
+	/*HashMap to calculate the Shortest interval between any two consecutive orders placed by the same customer*/
+	private HashMap<BigDecimal,Long> timeMap;
+	/*List to calculate Median order value*/
+	private List<BigDecimal> totalPrices;
+	/*HashMap to calculate Most and least frequently ordered items*/
+	private HashMap<BigDecimal,Integer> productMap;
+	/*boolean to determine the last page*/
 	private boolean loop;
 	private int page;
+	
+	private long totalOrder;
+	private long minInterval;
+	
+	private int maxOrder;
+	private List<BigDecimal> mostOrdered;
+	private List<BigDecimal> leastOrdered;
+	
 	public Assignment(){
 			 client = new OkHttpClient();
 			 gson = new Gson();
 			 uniqueCust = new HashSet<BigDecimal>();
-			 totalPrice = new ArrayList<BigDecimal>();
-			 //countTotal = new BigDecimal[1];
+			 totalPrices = new ArrayList<BigDecimal>();
+			 timeMap = new HashMap<BigDecimal, Long>();
+			 productMap = new HashMap<BigDecimal,Integer>();
 			 loop = true;
 			 page = 1;
+			 
+			 totalOrder = 0;
+			 minInterval = Long.MAX_VALUE;
+			 maxOrder = Integer.MIN_VALUE;
+			 mostOrdered = new ArrayList<BigDecimal>();
+			 leastOrdered = new ArrayList<BigDecimal>();
 		}
 		
-	 
+//	static class Pair{
+//		 Instant date;
+//		 long milliSecTime;
+//		 public Pair(Instant date, long milli){
+//			 this.date = date;
+//			 this.milliSecTime = milli;
+//		 }
+//	 }
 	 static class OrderContainer{
 		   private List<Order> orders;;
 		}
 	 
 	 static class Order{
-		private BigDecimal id;
+//		private BigDecimal id;
 		private String created_at;			
 		private String total_price;
 		private List<LineItem> line_items;
@@ -65,135 +89,104 @@ public class Assignment{
 		 private BigDecimal count;
 	 } 
 	 
-	 
-public void getTotalCount() throws Exception{
-	
-	//while(loop){
-	Request request = new Request.Builder()
-			  .url("https://100pure-demo.myshopify.com/admin/orders.json?status=any&page="+ page +"&fields=id,line_items,customer,total_price,created_at")
-			  .get()
-			  .addHeader("x-shopify-access-token", "b1ade8379e97603f3b0d92846e238ad8")
-			  .addHeader("cache-control", "no-cache")
-			  .addHeader("postman-token", "aa55cf3c-9688-b70d-0569-d69c77873ff2")
-			  .build();
-	 		
-	
-	 		Response response = client.newCall(request).execute();
-	 		
-	 		if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-	 		
-	 		OrderContainer gist = gson.fromJson(response.body().charStream(), OrderContainer.class);
-	        
-	        if( gist.orders == null || gist.orders.size() == 0){
-	        	loop = false; 
-	        	return;
-	        }else{
-	        	page++;
-	        	for (Order entry : gist.orders){
-			          System.out.println(entry.id);
-			          //System.out.print(entry.created_at);
-			          //System.out.println(Instant.parse(entry.created_at));
-			          String dateStr = entry.created_at;
-			          TemporalAccessor creationAccessor = DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(dateStr);
-			          Instant instant = Instant.from( creationAccessor );
-			          long millisSinceEpoch = instant.toEpochMilli( );
-			          System.out.println(millisSinceEpoch);
-			       
-			          System.out.println(entry.total_price);
-			          List<LineItem> list = entry.line_items;
-			          for(LineItem li : list)
-			        	  System.out.println(li.product_id);
-			          CustomerId custId = entry.customer;
-			          System.out.println(custId.id);
-			      }
-	        }
-	        response.close();
-	//}			
-
-}
-	 
- public void run() throws Exception {
- 
-	 while(loop){
-    	Request request = new Request.Builder()
-    			  .url("https://100pure-demo.myshopify.com/admin/orders.json?status=any&page="+page+"&fields=id,line_items,customer,total_price,created_at")
-    			  .get()
-    			  .addHeader("x-shopify-access-token", "b1ade8379e97603f3b0d92846e238ad8")
-    			  .addHeader("cache-control", "no-cache")
-    			  .addHeader("postman-token", "aa55cf3c-9688-b70d-0569-d69c77873ff2")
-    			  .build();
-
-		 client.newCall(request).enqueue(new Callback() {
-		      
-			 public void onFailure(Call call, IOException e) {
-		        e.printStackTrace();
-		        throw new RuntimeException(e);
-		      }
-
-		      public void onResponse(Call call, Response response) throws IOException {
-		        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+	public long getMillisecond(String date){
+		
+		TemporalAccessor creationAccessor = DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(date);
+	    Instant instant = Instant.from( creationAccessor );
+	    long millisSinceEpoch = instant.toEpochMilli( );
+		return millisSinceEpoch;
+	}
+	public void getTotalOrders() throws Exception{
+		
+	while(loop){
+		Request request = new Request.Builder()
+				  .url("https://100pure-demo.myshopify.com/admin/orders.json?status=any&page="+ page +"&fields=id,line_items,customer,total_price,created_at")
+				  .get()
+				  .addHeader("x-shopify-access-token", "b1ade8379e97603f3b0d92846e238ad8")
+				  .addHeader("cache-control", "no-cache")
+				  .addHeader("postman-token", "aa55cf3c-9688-b70d-0569-d69c77873ff2")
+				  .build();
+		 		
+		
+		 		Response response = client.newCall(request).execute();
+		 		
+		 		if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+		 		
+		 		OrderContainer gist = gson.fromJson(response.body().charStream(), OrderContainer.class);
 		        
-//		        Headers responseHeaders = response.headers();
-//		        for (int i = 0; i < responseHeaders.size(); i++) {
-//		          System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-//		        }
-
-		        OrderContainer gist = gson.fromJson(response.body().charStream(), OrderContainer.class);
-		        
-		        if( gist.orders.size() == 0){
+		        if( gist.orders == null || gist.orders.size() == 0){
 		        	loop = false; 
 		        	return;
 		        }else{
 		        	page++;
-		        	System.out.println("else process.");
-		        	
+		        	for (Order entry : gist.orders){
+				       
+		        			totalOrder++;
+				         
+		        			CustomerId custId = entry.customer;
+					        BigDecimal id = custId.id ;
+					        long millisSinceEpoch = getMillisecond(entry.created_at);
+					        if(timeMap.containsKey(id)){
+					        	
+					        	long timeSinceEpoch = timeMap.get(id);
+					        	long interval = Math.abs(millisSinceEpoch - timeSinceEpoch);
+					        	minInterval = (interval < minInterval) ? interval : minInterval;
+					        	
+					        }else{
+					        	timeMap.put(id,	millisSinceEpoch);
+					        }
+					        
+		        		 /* Creating a list of values*/
+		        		  String totalPrice = entry.total_price.replaceAll(",","");
+		        		  BigDecimal bd = new BigDecimal(totalPrice);
+		        		  totalPrices.add(bd);
+		        		  
+		        	     /* */
+				          List<LineItem> list = entry.line_items;
+				          for(LineItem li : list){
+						   
+						          if(productMap.containsKey(li.product_id)){
+						        	  productMap.put(li.product_id, productMap.get(li.product_id)+1);
+						        	  maxOrder = Math.max(maxOrder, productMap.get(li.product_id));
+						          }else{
+						        	  productMap.put(li.product_id,1);
+						          }
+				          }
+				          
+				          for(Map.Entry<BigDecimal, Integer> en : productMap.entrySet()){
+					        	  
+				        	  if(en.getValue() == maxOrder){
+					        		  mostOrdered.add(en.getKey());
+					        	  }else if(en.getValue() == 1){
+					        		  leastOrdered.add(en.getKey());
+					        	  }
+				        	  
+				          }
+				          
+				          
+				          	  
+				      }
 		        }
-		        	
-//		        for (Order entry : gist.orders){
-//			          System.out.println(entry.id);
-//			          System.out.println(entry.total_price);
-//			          List<LineItem> list = entry.line_items;
-//			          for(LineItem li : list)
-//			        	  System.out.println(li.product_id);
-//			          CustomerId custId = entry.customer;
-//			          System.out.println(custId.id);
-//			      }
-		        response.close(); 
-		      }
-		      
-		      
-		      });
-	 }
- }		      
+		        response.close();
+		}			
+	
+	}
+	 
+  
 public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		 Assignment ass = new Assignment();
-		 //ass.getTotalCount();
-		// System.out.println(ass.countTotal[0]);
+		 ass.getTotalOrders();
 		 
-		//while(ass.loop){
-			//ass.getTotalCount(); 
-			
-			
-		//System.out.println(ass.page);
-		// }
-		    //ass.run();
+		 System.out.println("Total No. of Orders : " + ass.totalOrder);
+		 System.out.println("No of Unique Customers: "+ ass.timeMap.size());
 		 
+		 Collections.sort(ass.totalPrices);
+		 int index = (int) Math.ceil((ass.totalPrices.size()+0.00)/2);
+		 System.out.println("Median Order Value: " + ass.totalPrices.get(index));
 		 
-		 String date1 = "2017-01-02T13:51:49-05:00";
-		 String date2 = "2016-12-07T14:42:19-05:00";
-		 TemporalAccessor creationAccessor1 = DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(date1);
-		 TemporalAccessor creationAccessor2 = DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(date2);
-         Instant instant1 = Instant.from( creationAccessor1 );
-         Instant instant2 = Instant.from( creationAccessor2 );
-        // System.out.println(instant1.compareTo(instant2));
-         long millisSinceEpoch1 = instant1.toEpochMilli( );
-         long millisSinceEpoch2 = instant2.toEpochMilli( );
-         System.out.println(millisSinceEpoch1);
-         System.out.println(millisSinceEpoch2);
-         System.out.println(millisSinceEpoch1 - millisSinceEpoch2);
-		 
-		 	
+		 System.out.println(  "Least Ordered Items ProductId :"+ Arrays.toString(ass.leastOrdered.toArray()));
+		 System.out.println(  "Most Ordered Items ProductId  :"+ Arrays.toString(ass.mostOrdered.toArray()));
 	}
 
  }
